f50ec486b8c6c654d3c36b814b36270a15b7030e
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2024-02-21 17:43:01 -0300

    Configurações iniciais do projeto

---
+++ src/test/java/com/raphael/msuser/MsuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
