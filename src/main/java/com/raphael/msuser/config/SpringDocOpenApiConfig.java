1943e258d94787acd794a5ebdb8f9646ee0014c8
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2024-02-26 12:28:02 -0300

    Adicionando classe de configuração do SpringDoc/Swagger

---
+++ src/main/java/com/raphael/msuser/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.raphael.msuser.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(
+                        new Info()
+                                .title("REST API - Gerenciamento de usuários")
+                                .description("API para gestão de usuários")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Raphael Araújo").email("raphael.araujo.pb@compasso.com.br"))
+                );
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .description("Insira um bearer token válido para prosseguir")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
+}
